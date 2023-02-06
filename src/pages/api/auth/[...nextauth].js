7d051e0e8437d2cdf811a9141f562c0626763ea4
gloria-tampuri <gloriatampuri15@gmail.com>
2023-02-06 11:12:22 +0000

    back to local

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -19,11 +19,6 @@ providers: [
 ],
 secret:process.env.JWT_SECRET,
    callbacks: {
-    redirect:async(url, baseUrl)=>{
-      return url.startsWith(baseUrl)
-      ? Promise.resolve(url)
-      : Promise.resolve(baseUrl)
-    },
       async signIn({ user, account, profile, email, credentials }) {
         if (!user?.role) {
           user.role = "USER";
