a0510d8a51abe92261a3aca5333ef027cc8fff17
gloria-tampuri <gloriatampuri15@gmail.com>
2023-02-17 18:11:16 +0000

    summary done

--- Components/Equipment/IndividualEquipment/EquipmentSummary.js
+++ Components/Equipment/IndividualEquipment/EquipmentSummary.js
@@ -3,18 +3,42 @@ import classes from './Summary.module.css'
 
 const EquipmentSummary = ({equipment}) => {
 
+  const[totalInflow, setTotalInflow]=useState(0)
+   const [totalExpenditure, setTotalExpenditure] = useState(0)
+  const [totalMiscellaneous, setTotalMiscellaneous] = useState(0)
+
+  useEffect(()=>{
+    if (equipment != null || equipment !== undefined) {
+      const totalInflowAmount = equipment?.inflows?.map(inflow => +inflow.amount).reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0)
+
+      const totalExpenditureAmount = equipment?.expenditure?.map(expenditure => +expenditure.amount).reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0)
+
+        const totalMiscellaneousAmount= equipment?.miscellaneous?.map(miscellaneous => +miscellaneous.amount).reduce(
+          (accumulator, currentValue) => accumulator + currentValue,
+          0 
+        )
+
+        setTotalInflow(totalInflowAmount)
+      setTotalExpenditure(totalExpenditureAmount)
+      setTotalMiscellaneous(totalMiscellaneousAmount)
+          }
+  },[equipment])
 
   return (
     <div>
            <ul className={classes.sumarryList}>
-        {/* <li>Crop Number: <span>{crop && crop.cropId}</span></li> */}
+        {/* <li>equipment Number: <span>{crop && crop.cropId}</span></li> */}
         <li>Equipment Name: <span>{equipment&&equipment.equipmentType}</span></li>
         <li>Model: <span>{equipment&&equipment.model}</span></li>
         <li>Make Year: <span>{equipment&&equipment.makeYear}</span></li>
         <li>Date Purchased: <span>{equipment&&equipment.datePurchased}</span></li>
-        <li>Inflows:<span></span></li>
-        <li>Expenditure:<span></span></li>
-        <li>Miscellaneous:<span></span></li>
+        <li>Inflows:<span>{totalInflow}</span></li>
+        <li>Expenditure:<span>{totalExpenditure}</span></li>
+        <li>Miscellaneous:<span>{totalMiscellaneous}</span></li>
       </ul>
     </div>
   )
