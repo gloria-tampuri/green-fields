268c16dc4b6bebeabab5cbd402733869c0aa46c6
gloria-tampuri <gloriatampuri15@gmail.com>
2023-02-21 16:06:01 +0000

    animate

--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -5,28 +5,68 @@ import Header from 'Components/Header/Header'
 import google from '/public/google.png'
 import Logo from '/public/greenfieldsLogo.png'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 const Login = () => {
   const { data: session } = useSession()
 const currentYear = new Date().getFullYear()
+const loginVariant={
+  hidden:{
+    // background:'white',
+  },
+
+  show:{
+    // background:'red'
+  }
+}
+const logoVariant={
+  hidden:{
+    opacity:0,y:'-100px'
+  },
+  show:{
+    opacity:1,y:0,
+    transition:{
+    duration:0.2, delay:1
+  }
+  }
+}
+
+const googleVariant={
+  hidden:{ opacity:0,y:'200px'},
+  show:{
+    opacity:1,y:0,
+    transition:{
+    duration:0.2, delay:1
+  }
+  }
+}
 
   return (
-    <div className={classes.login}>
+    <motion.div variants={loginVariant} 
+    initial='hidden'
+    animate='show'
+    className={classes.login}>
       {/* <Header /> */}
-      <div className={classes.logoBanner}>
+      <motion.div variants={logoVariant} 
+      
+      
+      className={classes.logoBanner}>
       <Image className={classes.Logo} src={Logo} alt='logo' width={250} height={250}/>
-      </div>
+      </motion.div>
       <h1 className={classes.welcome}>
         Welcome To Green-Fields Staff Portal
       </h1>
-      <div onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/dashboard' })} className={classes.medium}>
+
+      <motion.div 
+      variants={googleVariant}
+      onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/dashboard' })} className={classes.medium}>
         <p> Sign in with</p>
         <Image src={google} alt='google sign' width={30} height={30} />
-      </div>
+      </motion.div>
       <div className={classes.footer}>
         <p>Green-Fields @{currentYear}</p>
       </div>
-    </div>
+    </motion.div>
   )
 
 }
